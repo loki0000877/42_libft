845105b5d4c5ca41aa9295514f0075d883054074
Richard Bruch <richardbruch1@gmail.com>
2026-06-16 15:54:46 +0200

    Update ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,21 +1,31 @@
-char  *ft_strdup(const char *s)
-{
-  char    *s1;
-  size_t  len;
-  size_t   i;
+#include "libft.h"
 
-  len = 0;
-  while (s[len])
-    len++;
-  s1 = malloc(len + 1); 
-  if (!s1)
-    return (NULL);
-  i = 0;
-  while (i < len)
-  {
-    s1[i] = s[i];
-    i++;
-  }
-  s1[i] = '\0';
-  return (s1);
+char    *ft_strdup(const char *s)
+{
+    char    *ptr;
+    size_t      size;
+    size_t      i;
+
+    size = ft_strlen(s);
+    ptr = malloc(size + 1);
+    if (!ptr)
+        return (NULL);
+    i = 0;
+    while (i < size)
+    {
+        ptr[i] = s[i];
+        i++;
+    }
+    ptr[i] = '\0';
+    return (ptr);
+}
+
+size_t  ft_strlen(const char *s)
+{
+    size_t  i;
+
+    i = 0;
+    while (s[i])
+        i++;
+    return (i);
 }
