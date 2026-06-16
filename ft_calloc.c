86f2c6436b67102a4d9dd4c5a48f08f504f45fcc
Richard Bruch <richardbruch1@gmail.com>
2026-06-16 15:42:31 +0200

    Update ft_calloc.c
    add variant

--- ft_calloc.c
+++ ft_calloc.c
@@ -20,3 +20,36 @@ void  *ft_calloc(size_t nmemb, size_t size)
   }
   return(ptr);
 }
+
+
+
+#include <stddef.h>
+#include "libft.h"
+
+void    *ft_calloc(size_t nmemb, size_t size)
+{
+    void    *ptr;
+
+    if (size > 0 && nmemb > (SIZE_MAX / size))
+        return (NULL);
+    ptr = malloc(nmemb * size);
+    if (!ptr)
+        return (NULL);
+    ft_bzero(ptr, nmemb * size);
+    return (ptr);
+}
+
+void    ft_bzero(void *s, size_t n)
+{
+    unsigned char   *p;
+    size_t  i;
+
+    i = 0;
+    p = (unsigned char *)s;
+    while (i < n)
+    {
+        p[i] = 0;
+        i++;
+    }
+}
+
